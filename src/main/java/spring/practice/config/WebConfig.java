52dcf51242f32603c4235ab477f1cca1fd44af67
Alex Korzun <koleksandr4@gmail.com>
2021-02-10 15:57:46 +0200

    added UserResponseDto

--- src/main/java/spring/practice/config/WebConfig.java
+++ src/main/java/spring/practice/config/WebConfig.java
@@ -2,8 +2,10 @@ package spring.practice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = {"spring.practice.controller"})
 public class WebConfig {
 }
--- src/main/java/spring/practice/controller/UserController.java
+++ src/main/java/spring/practice/controller/UserController.java
@@ -1,8 +1,13 @@
 package spring.practice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+    @GetMapping(value = "/hello")
+    public String sayHello() {
+        return "hello";
+    }
 }
