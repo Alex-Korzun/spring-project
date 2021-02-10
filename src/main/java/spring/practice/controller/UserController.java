0207942903d71028de71ce653d011ae740c8b7c9
Alex Korzun <koleksandr4@gmail.com>
2021-02-10 18:35:41 +0200

    added methods to UserController

--- src/main/java/spring/practice/controller/UserController.java
+++ src/main/java/spring/practice/controller/UserController.java
@@ -2,23 +2,51 @@ package spring.practice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.practice.dto.UserResponseDto;
+import spring.practice.model.User;
 import spring.practice.service.UserService;
+import spring.practice.service.impl.UserMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
+    private final UserMapper userMapper;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserMapper userMapper) {
         this.userService = userService;
+        this.userMapper = userMapper;
     }
 
     @GetMapping("/{userId}")
-    public UserResponseDto get(Long userId) {
+    public UserResponseDto get(@PathVariable Long userId) {
+        return userMapper.toDto(userService.get(userId));
+    }
 
+    @GetMapping()
+    public List<UserResponseDto> getAll() {
+        return userService.listUsers()
+                .stream()
+                .map(userMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/inject")
+    public void inject() {
+        User bob = new User();
+        bob.setEmail("bob@gmail.com");
+        User alice = new User();
+        alice.setEmail("alice@gmail.com");
+        User john = new User();
+        john.setEmail("john.gmail.com");
+        User kyle = new User();
+        kyle.setEmail("kyle@gmail.com");
     }
 }
--- src/main/java/spring/practice/service/impl/UserMapper.java
+++ src/main/java/spring/practice/service/impl/UserMapper.java
@@ -1,17 +1,19 @@
 package spring.practice.service.impl;
 
+import org.springframework.stereotype.Component;
 import spring.practice.dto.UserResponseDto;
 import spring.practice.model.User;
 
+@Component
 public class UserMapper {
-    public static UserResponseDto toDto(User user) {
+    public  UserResponseDto toDto(User user) {
         UserResponseDto dto = new UserResponseDto();
         dto.setId(user.getId());
         dto.setEmail(user.getEmail());
         return dto;
     }
 
-    public static User fromDto (UserResponseDto userResponseDto) {
+    public  User fromDto (UserResponseDto userResponseDto) {
         User user = new User();
         user.setId(userResponseDto.getId());
         user.setEmail(userResponseDto.getEmail());
